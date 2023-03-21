be40e4cf3a0bb507fce478ab76a4444151e017a2
BATTEPATI-LOKESH-REDDY <125631854+BATTEPATI-LOKESH-REDDY@users.noreply.github.com>
2023-03-21 18:58:34 +0530

    Task- completed

---
+++ task-4/patterns/builder/Wrapper.java
@@ -0,0 +1,11 @@
+package patterns.builder;
+
+public class Wrapper implements Packing{
+
+	@Override
+	public String pack() {
+		// TODO Auto-generated method stub
+		return "wrapper packing";
+	}
+
+}
---
+++ task-4/patterns/factory/Circle.java
@@ -0,0 +1,11 @@
+package patterns.factory;
+
+public class Circle implements Shape{
+
+	@Override
+	public void draw() {
+		System.out.println("Inside the Circle draw() method");
+		
+	}
+
+}
